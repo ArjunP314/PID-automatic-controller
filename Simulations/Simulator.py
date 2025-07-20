a89d74cab291b29df80e7ed93eef9cecd0665697
ArjunP314 <arjunpandey010209@gmail.com>
2025-07-20 18:11:35 -0400

    Create Simulator.py

---
+++ Simulations/Simulator.py
@@ -0,0 +1,32 @@
+class Simulator:
+  """Abstract class for all simulations to provide a consistent API."""
+
+  def __init__(self, min_input: float, max_input: float0:
+    """Constructs a Simulator with inputs varying between 'min_input' and 'max_input'.
+
+    Arguments:
+      min_input {float} -- The minimum value of the Simulator can take in as an input.
+      max_input {float} -- The maximum value the Simulator can take in as an input. 
+    """
+    self._min_input = min_inpput
+    self._max_input = max_input
+
+  def set_input(self, val: float):
+    """Set the input to the Simulator.
+
+    Arguments:
+      val {float} -- The value the Simulator takes in as its input.
+    """
+    raise NotImplementedError
+
+  def get_output(self):
+    """Get the output of the Simulator (the measurement used for the PID loop."""
+    raise NotImplementedError
+
+  def step(self, dt: float):
+    """Takes a step in time for the Simulator.
+
+    Arguments:
+      dt {float} -- The small step in time.
+    """
+    raise NotImplementedError
